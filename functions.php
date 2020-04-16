f5819c6ef63f0abf6159827ae517f948a6654c90
Kent vejrup Madsen <Kent.vejrup.madsen@goalpioneers.dk>
2020-04-17 00:30:35 +0200

    a

--- functions.php
+++ functions.php
@@ -1,3 +1,18 @@
 <?php
  
+ 
+function goalpioneers_theme_support() 
+{
+
+}
+ 
+add_action( 'after_setup_theme', 'goalpioneers_theme_support' );
+
+
+function goalpioneers_register_styles() 
+{
+}
+
+add_action( 'wp_enqueue_scripts', 'goalpioneers_register_styles' );
+
 ?>
