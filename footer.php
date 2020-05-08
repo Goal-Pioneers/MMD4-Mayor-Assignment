e44636db4a6b06c1861611f4cb1573832e434184
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-05-08 12:59:59 +0200

    organising code

--- footer.php
+++ footer.php
@@ -63,36 +63,12 @@
     </footer>
 </body>
 
-<!-- Matomo -->
-<script type="text/javascript">
-    var _paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-        var u="//matomo.goalpioneers.dk/";
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
-        _paq.push(['setSiteId', '1']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-    })();
-</script>
-<!-- End Matomo Code -->
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160026683-1">
-</script>
-
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-160026683-1');
-    </script>
-
-
-    <?php 
+<?php 
         include 'development.php';
-    ?>
+?>
 </html>
+
+<?php 
+require get_parent_theme_file_path('/include/analytics/google-analytics.php');
+require get_parent_theme_file_path('/include/analytics/matomo-analytics.php');
+?>
