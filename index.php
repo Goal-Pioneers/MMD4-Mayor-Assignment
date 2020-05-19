0c597f788f31ef3ba95d03bbf58eddc9eb0d9dac
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-05-19 09:52:43 +0200

    current changes

--- index.php
+++ index.php
@@ -9,13 +9,16 @@
                                 if( is_front_page() ):
                         ?>
                         
-                         <?php require get_parent_theme_file_path('/include/templates/frontpage/component-products.php'); ?>
-                         <?php require get_parent_theme_file_path('/include/templates/frontpage/component-services.php'); ?>
+                                <?php require get_parent_theme_file_path('/include/templates/frontpage/component-products.php'); ?>
+                                <?php require get_parent_theme_file_path('/include/templates/frontpage/component-services.php'); ?>
+                                
+                                <?php require get_parent_theme_file_path('/include/templates/frontpage/component-articles.php'); ?>
+                                <?php require get_parent_theme_file_path('/include/templates/frontpage/component-about-us.php'); ?>
+                                <?php require get_parent_theme_file_path('/include/templates/frontpage/component-contact.php'); ?>
                         
-                         <?php require get_parent_theme_file_path('/include/templates/frontpage/component-articles.php'); ?>
-                         <?php require get_parent_theme_file_path('/include/templates/frontpage/component-about-us.php'); ?>
-                         <?php require get_parent_theme_file_path('/include/templates/frontpage/component-contact.php'); ?>
-                        <?php endif; ?>
+                        <?php 
+                                endif; 
+                        ?>
                 
                 
 
