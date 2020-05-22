47de11e0cc775c38e0f06d121cc4030790a64b07
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-05-22 16:00:45 +0200

    current

--- include/templates/specifics/frontpage/component-articles.php
+++ include/templates/specifics/frontpage/component-articles.php
@@ -1,29 +1,55 @@
 
-                        <section id="news-component" class="base-section"> 
-                                <h2> 
-                                Artikler
-                                </h2>
+<section id="news-component" class="base-section"> 
+        <h2> 
+                Artikler
+        </h2>
 
-                                <ul class="articles-container"> 
-                                        <span class="listed"> 
-                                                <li> 
-                                                        2
-                                                
-                                                </li>
-                                                
-                                                <li> 
-                                                        3
-                                                </li>
-                                                
-                                                <li> 
-                                                        4
-                                                </li>
+        <ul class="articles-container"> 
+                <span class="listed">
+                        <?php 
 
-                                                <li> 
-                                                        5
-                                                </li>
-                                        </span>
-                                </ul>
+                        ?> 
 
-                        </section>
-                
+                        <?php 
+                                $args_post = 
+                                array(
+                                        "posts_per_page" => 4,
+                                        "paged" => 1,
+                                        "tax_query" => array(
+                                                array(
+                                                        "taxonomy" => "category",
+                                                        "field"    => "slug",
+                                                        "terms"    => "artikel",
+                                                )
+                                        ),
+                                        "orderBy" => "post_date",
+                                        "order" => "desc",
+                                        "post_type" => "post",
+                                        "post_status" => "publish"
+                                );
+
+                                $posts_array = get_posts($args_post);
+                        ?>
+
+                        <?php foreach($posts_array as &$element): ?>
+                                <li>
+                                        <a href="<?php echo the_permalink($element->ID); ?>"> 
+                                                <div> 
+                                                        <img src="<?php echo get_the_post_thumbnail_url($element->ID, 'full'); ?>"/>                                                
+                                                        <div class="text">
+                                                                <p> 
+                                                                        <?php /* echo get_the_category($element->ID); */ ?>
+                                                                        Test
+                                                                </p>
+                                                                <h3> 
+                                                                        <?php echo get_the_title($element->ID); ?>
+                                                                </h3>
+                                                                
+                                                        </div>
+                                                </div>
+                                        </a>
+                                </li>
+                        <?php endforeach;?>
+                </span>
+        </ul>
+</section>
