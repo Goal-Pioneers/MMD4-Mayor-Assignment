51d42c179bcac435d991ee6f7370b27b314c77b1
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-05-22 10:28:31 +0200

    current

--- include/templates/specifics/frontpage/component-products.php
+++ include/templates/specifics/frontpage/component-products.php
@@ -10,17 +10,33 @@
                         $children = get_children( $post->ID );
                 ?>
                 <?php if( !( $children == null ) ): ?>
-                        <?php foreach($children as &$child): ?>
+                        <?php $n = 0; ?>
+
+                        <?php foreach( $children as &$child ): ?>
+                                <?php 
+                                        if ( $n == 3 )
+                                        {
+                                                break;
+                                        }
+                                ?>
+                                
                                 <li class="product"> 
-                                        <a href="<?php echo get_post_permalink($child->ID); ?>">
+                                        <a href="<?php echo get_post_permalink( $child->ID ); ?>">
                                                 <div class="product-container">
                                                         <img src="<?php echo get_the_post_thumbnail_url( $child->ID ); ?>" class="image" />
                                                          
-                                                        <h3> <?php echo get_the_title( $child->ID ); ?> </h3>
-                                                        <p> <?php echo get_the_excerpt( $child->ID ); ?> </p>
+                                                        <h3>
+                                                                <?php echo get_the_title( $child->ID ); ?> 
+                                                        </h3>
+
+                                                        <p>
+                                                                <?php echo get_the_excerpt( $child->ID ); ?>
+                                                        </p>
                                                 </div>
                                         </a>
                                 </li>
+
+                                <?php $n = $n + 1; ?>
                         <?php endforeach; ?>
                 <?php endif; ?>
         </ul>
