d3dd714df5126171a27a5150ce61bda208c01150
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-05-25 00:20:37 +0200

    current

--- sidebar.php
+++ sidebar.php
@@ -1,28 +1,36 @@
 <?php ?>
 
     <div class="sidebar"> 
-        <div class="ui-sidebar-button hide-on-desktop" id="ui-action-show-menu" onclick=""> 
+        <div class="ui-sidebar-button hide-on-desktop" 
+             id="ui-action-show-menu" 
+             onclick="showUINavigation();"> 
             <span> 
                 <i class="fas fa-bars">
                 </i>
             </span>
         </div>
 
-        <div class="ui-sidebar-button" id="ui-action-question" onclick=""> 
+        <div class="ui-sidebar-button" 
+             id="ui-action-question" 
+             onclick=""> 
             <span> 
                 <i class="fas fa-question">
                 </i>
             </span>
         </div>
 
-        <div class="ui-sidebar-button" id="ui-action-adjust-light" onclick=""> 
+        <div class="ui-sidebar-button" 
+             id="ui-action-adjust-light" 
+             onclick="adjustLight();"> 
             <span> 
                 <i class="fas fa-adjust">
                 </i>
             </span>
         </div>
 
-        <div class="ui-sidebar-button" id="ui-action-go-to-top" onclick="goToTopOfPage();"> 
+        <div class="ui-sidebar-button" 
+             id="ui-action-go-to-top" 
+             onclick="goToTopOfPage();"> 
             <span> 
                 <i class="fas fa-chevron-down">
                 </i>
