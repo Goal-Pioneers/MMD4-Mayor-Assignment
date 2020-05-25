56520646f1ecea7cb09b8a5456cf25171e647c46
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-05-25 19:06:26 +0200

    a

--- page-products.php
+++ page-products.php
@@ -10,32 +10,36 @@
                 <?php if( !( $children == null ) ): ?>
                         <?php $n = 0; ?>
 
-                        <?php foreach( $children as &$child ): ?>
-                                <?php 
-                                        if ( $n == 3 )
-                                        {
-                                                break;
-                                        }
-                                ?>
-                                
-                                <li class=""> 
-                                        <a href="<?php echo get_post_permalink( $child->ID ); ?>">
-                                                <div class="">
-                                                        <img src="<?php echo get_the_post_thumbnail_url( $child->ID ); ?>" class="image" />
-                                                         
-                                                        <h3>
-                                                                <?php echo get_the_title( $child->ID ); ?> 
-                                                        </h3>
+                        <ul class="page-products-page"> 
+                                <?php foreach( $children as &$child ): ?>
+                                        <?php 
+                                                if ( $n == 3 )
+                                                {
+                                                        break;
+                                                }
+                                        ?>
+                                        
+                                                <li class=""> 
+                                                        <a href="<?php echo get_post_permalink( $child->ID ); ?>">
+                                                                <div class="page-product-element">
+                                                                        <img src="<?php echo get_the_post_thumbnail_url( $child->ID ); ?>" class="image" />
+                                                                        
+                                                                        <div class="product-text-container"> 
+                                                                                <h3>
+                                                                                        <?php echo get_the_title( $child->ID ); ?> 
+                                                                                </h3>
 
-                                                        <p>
-                                                                <?php echo get_the_excerpt( $child->ID ); ?>
-                                                        </p>
-                                                </div>
-                                        </a>
-                                </li>
+                                                                                <p>
+                                                                                        <?php echo get_the_excerpt( $child->ID ); ?>
+                                                                                </p>
+                                                                        </div>
+                                                                </div>
+                                                        </a>
+                                                </li>
 
-                                <?php $n = $n + 1; ?>
-                        <?php endforeach; ?>
+                                        <?php $n = $n + 1; ?>
+                                <?php endforeach; ?>
+                        </ul>
                 <?php endif; ?>
         </main>
 
