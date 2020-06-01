4799f6cacddb96b6cf5cf3784d973518bdd8f4eb
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-06-01 19:10:41 +0200

    removed file

--- page-glossary.php
+++ page-glossary.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?> 
-
         <main id="main-view"> 
                 <section id="faq">
                         <h1> <?php the_title(); ?> </h1>
@@ -11,6 +10,5 @@
                 </section>
     
         </main>
-
 <?php get_footer(); ?>
     
