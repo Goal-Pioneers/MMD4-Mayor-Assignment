add99b222cdb8639db0db8142b40ccee699e4ce6
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-06-01 22:22:14 +0200

    splitting the webpage into templtes

--- header.php
+++ header.php
@@ -3,10 +3,7 @@
 <html <?php language_attributes(); ?> id="view">
     <head>
         <?php require get_parent_theme_file_path('/include/templates/generic/head.php'); ?>
-
-        <?php if( is_front_page() ): ?>
-
-        <?php endif; ?>
+        
     </head>
 
     <body onload="">
