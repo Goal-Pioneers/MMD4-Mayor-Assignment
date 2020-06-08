b80ac25634d100689b9858f8faa7976c6c15478f
Kent vejrup Madsen <kent.vejrup.madsen@outlook.com>
2020-06-08 23:53:28 +0200

    added area in bottom for the cookie policy

---
+++ include/templates/footer/component-cookie.php
@@ -0,0 +1,38 @@
+<div id="cookie-policy">
+    <!-- -->
+    <div> 
+        <p> Vi anvender cookie på denne side. </p>
+        <ul> 
+            <li> til teknisk funktionalit til at have siden kørende. </li>
+            <li> til marketing for at finde ud af brugs adfærd </li>
+        </ul>
+    </div>
+
+    <form> 
+
+        <!-- -->
+        <div> 
+            <input type="checkbox" id="cookies-func" name="functional" value="" checked>
+            <label for="functional"> Basis funktionalitet </label> 
+            
+            <input type="checkbox" id="cookies-marketing" name="marketing" value="">
+            <label for="marketing"> Marketing </label>
+            
+        </div>
+            
+            <!-- -->
+        <div>
+            <div class="btn btn-primary"> 
+                Accept All
+            </div>  
+        
+            <div class="btn btn-primary"> 
+                Accept
+            </div>
+                
+            <div class="btn btn-primary"> 
+                Deny All
+            </div>
+        </div>
+    </form>
+</div>
